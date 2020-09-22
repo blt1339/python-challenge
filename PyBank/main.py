f510b6567dd4e016150e4fdad57a008ca5926a6b
Beverly Thompson <blt1339@gmail.com>
2020-09-22 12:01:38 -0400

    Modify csvfile loop the calculate the need information and display to the screen
    Modify csvfile loop the calculate the need information and display to the screen.  I deleted the dictionoary with period as the key because I went a different route and there was no need for it.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,32 +1,68 @@
 import csv
+from statistics import mean
 from os import path
 
-
 # setup the filepath for the csv file we want to read
-filename = "budget_data.csv"
+csvfilename = "budget_data.csv"
+#filepath = path.join("..", "Resources", filename)
 basepath = path.dirname(__file__)
-filepath = path.abspath(path.join(basepath, "Resources", filename))
+csvfilepath = path.abspath(path.join(basepath, "Resources", csvfilename))
 
 # Initialize variables
 no_months = 0
-net_profit_loss = 0
-profit_loss = {}
+net_pl_amount = 0
 
-with open(filepath, 'r') as csv_file:
+with open(csvfilepath, 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     
     # Go by the header row
     next(csv_reader)
-    
+
+    # Initialize loop variables
+    prev_pl_month = None
+    prev_pl_amount = None
+    increase_pl_month = None
+    increase_pl_amount_change = None
+    decrease_pl_month = None
+    decrease_pl_amount_change = None
+    pl_amount_changes = []
+
     for row in csv_reader:
         no_months += 1
 
-        month = row[0]
-        profit_loss_for_month = int(row[1])
-        profit_loss[month] = profit_loss_for_month   
-            
-        net_profit_loss += profit_loss_for_month
+        pl_month = row[0]
+        pl_amount = int(row[1])
 
-    print(no_months)
-    print(net_profit_loss)
-    print(profit_loss)
+        net_pl_amount += pl_amount
+
+        if prev_pl_month != None:
+            pl_amount_change = pl_amount - prev_pl_amount
+            pl_amount_changes.append(pl_amount_change)
+
+            if pl_amount_change > 0:
+                if increase_pl_month == None:
+                    increase_pl_month = pl_month
+                    increase_pl_amount_change = pl_amount_change
+                else:
+                    if pl_amount_change > increase_pl_amount_change:
+                        increase_pl_month = pl_month
+                        increase_pl_amount_change = pl_amount_change
+            else:
+                if decrease_pl_month == None:
+                    decrease_pl_month = pl_month
+                    decrease_pl_amount_change = pl_amount_change
+                else:
+                    if pl_amount_change < decrease_pl_amount_change:
+                        decrease_pl_month = pl_month
+                        decrease_pl_amount_change = pl_amount_change
+        
+        prev_pl_month = pl_month
+        prev_pl_amount = pl_amount
+
+print("Financial Analysis")
+print('------------------')
+print("Total Months: {}".format(no_months))
+print("Total: ${}".format(net_pl_amount))
+print("Average  Change: ${}".format(round(mean(pl_amount_changes),2)))
+print("Greatest Increase in Profits: {} (${})".format(increase_pl_month,increase_pl_amount_change))
+print("Greatest Decrease in Profits: {} (${})".format(decrease_pl_month,decrease_pl_amount_change))
