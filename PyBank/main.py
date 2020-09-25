536ba37e81ea306c064358f02b33c7e07b26a9c0
Beverly Thompson <blt1339@gmail.com>
2020-09-25 13:23:34 -0400

    Go through PyBank and add comments where missing and necessary
    Go through PyBank and add comments where missing and necessary.

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,17 +2,22 @@ import csv
 from statistics import mean
 from os import path
 
-# setup the filepath for the csv file we want to read
-csv_file_name = "budget_data.csv"
-results_file_name = "PyBank_Results.txt"
+# Get the base path
 base_path = path.dirname(__file__)
+
+# Setup the filepath for the csv file we want to read
+csv_file_name = "budget_data.csv"
 csv_file_path = path.abspath(path.join(base_path, "Resources", csv_file_name))
+
+# Setup the filepath for the results file we want to read
+results_file_name = "PyBank_Results.txt"
 results_file_path = path.abspath(path.join(base_path,"Analysis",results_file_name))
 
 # Initialize variables
 no_months = 0
 net_pl_amount = 0
 
+# Open the csv file
 with open(csv_file_path, 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     
@@ -28,14 +33,24 @@ with open(csv_file_path, 'r') as csv_file:
     decrease_pl_amount_change = None
     pl_amount_changes = []
 
+    # Loop through the rows of data
     for row in csv_reader:
+
+        # Increase the month counter
         no_months += 1
 
+        # grab the month and the amount from the row of data
         pl_month = row[0]
         pl_amount = int(row[1])
 
+        # Add row amount into the net total amount
         net_pl_amount += pl_amount
 
+        # If we are not on the first row of data than calculate 
+        # the difference, store that difference so we can take
+        # an average when we are done and see if change is 
+        # either greatest increase or decrease and if it is 
+        # then update respective variables
         if prev_pl_month != None:
             pl_amount_change = pl_amount - prev_pl_amount
             pl_amount_changes.append(pl_amount_change)
@@ -56,10 +71,14 @@ with open(csv_file_path, 'r') as csv_file:
                     if pl_amount_change < decrease_pl_amount_change:
                         decrease_pl_month = pl_month
                         decrease_pl_amount_change = pl_amount_change
-        
+
+        # Now that we are done processing for this row, save the 
+        # month and amount in previous variables to be utilzed on
+        # the next loop to calculate the change        
         prev_pl_month = pl_month
         prev_pl_amount = pl_amount
 
+# Now that we have gone through all of the data, output the results
 output = []
 output.append("Financial Analysis")
 output.append('------------------')
@@ -70,7 +89,7 @@ output.append("Greatest Increase in Profits: {} (${})".format(increase_pl_month,
 output.append("Greatest Decrease in Profits: {} (${})".format(decrease_pl_month,decrease_pl_amount_change))
 
 
-
+# An finally write out results to a file
 with open(results_file_path, 'w') as results_file:
     for output_line in output:
         print(output_line)
