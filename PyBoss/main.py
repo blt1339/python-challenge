b2633c2d23b37374549232f06b1423828673fa31
Beverly Thompson <blt1339@gmail.com>
2020-09-27 12:09:39 -0400

    Organize code in PyBoos main.py and add any missing comments
    Organize code and add any missing comments

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -4,7 +4,7 @@ import datetime
 
 # Get the base path
 base_path = path.dirname(__file__)
-
+    
 # Setup the filepath for the csv file we want to read
 csv_file_name = "employee_data.csv"
 csv_file_path = path.abspath(path.join(base_path, "Resources", csv_file_name))
@@ -66,36 +66,76 @@ us_state_abbrev = {
     'Wisconsin': 'WI',
     'Wyoming': 'WY',
 }
+
 # Initialize list to store converted data
 output_data = []
 
 # Add a row of headings for the first row of our list to store converted data
 output_data.append(["Emp ID","First Name","Last Name","DOB","SSN","State"])
 
+def file_read():
+    '''
+    Read the csv file that is setup in csv_file_path
+    '''
 
-# Open the csv file
-with open(csv_file_path, 'r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    # Open the csv file
+    with open(csv_file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
     
-    # Go by the header row
-    headers = next(csv_reader)
+        # Go by the header row
+        next(csv_reader)
 
-    # Loop through the rows of data
-    for row in csv_reader:
-        emp_id = row[0]
-        name = row[1]
-        first_name, last_name = name.split()
-        dob = row[2]
-        output_dob = datetime.datetime.strptime(dob, "%Y-%m-%d").strftime("%m/%d/%Y")
-        ssn = row[3]
-        output_ssn = "***-**-" + ssn.split('-')[2]
-        state = row[4]
-        state_abbrev = us_state_abbrev[state]
+        # Loop through the rows of data
+        for row in csv_reader:
+            # Get employee id
+            emp_id = row[0]
 
-        # Add converted data to output_data
-        output_data.append([emp_id,first_name,last_name,output_dob,output_ssn,state_abbrev])
+            # Get name and split into first and last name
+            name = row[1]
+            first_name, last_name = name.split()
 
-# An finally write out the converted data
-with open(export_file_path, 'w') as export_file:
-    csv_writer = csv.writer(export_file)
-    csv_writer.writerows(output_data)
+            # Get date of birth and switch format to mm/dd/yyyy
+            dob = row[2]
+            output_dob = datetime.datetime.strptime(dob, "%Y-%m-%d").strftime("%m/%d/%Y")
+
+            # Get the social security number and * out the first two parts of the number
+            ssn = row[3]
+            output_ssn = "***-**-" + ssn.split('-')[2]
+            # Translate the state from full state name to two character state abbreviations
+            state = row[4]
+            state_abbrev = us_state_abbrev[state]
+
+            # Add converted data to output_data
+            output_data.append([emp_id,first_name,last_name,output_dob,output_ssn,state_abbrev])
+
+
+def file_write():
+    '''
+    Write out the converted data to export_file_path
+    '''
+
+    # Write out the converted data
+    with open(export_file_path, 'w') as export_file:
+        csv_writer = csv.writer(export_file)
+        csv_writer.writerows(output_data)
+
+def main():
+    '''
+    Read the data from employee_data.csv, separate the name into first and last, 
+    reformat the date to mm/dd/yyyy, * the numbers in the first 2 parts of the
+    social security number, and translate state name to state abbrev before outputting 
+    to file PyBoss_Export.csv.
+    '''
+    
+    # Read through the csv data and create output_date with converted data 
+    file_read()
+
+    # Write out the converted data
+    file_write()
+
+    # Output that we have created the file
+    print('--->  The file {} has been created <--'.format(export_file_path))
+
+if __name__ == "__main__":
+    main()
