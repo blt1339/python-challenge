155a9fc2644334813ee93eafd9e71bd667209a9e
Beverly Thompson <blt1339@gmail.com>
2020-09-27 11:02:25 -0400

    Add logic to extract the data, reformat the data for output and print to verify code
    Add logic to extract the data, reformat the data for output and print to verify code.

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -13,6 +13,60 @@ csv_file_path = path.abspath(path.join(base_path, "Resources", csv_file_name))
 results_file_name = "PyBoss_Export.csv"
 results_file_path = path.abspath(path.join(base_path,"Analysis",results_file_name))
 
+# Setup a dictionary to translate to 2 character states
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
 # Initialize variables
 no_months = 0
 net_pl_amount = 0
@@ -26,5 +80,13 @@ with open(csv_file_path, 'r') as csv_file:
 
     # Loop through the rows of data
     for row in csv_reader:
-        print(row)
-
+        emp_id = row[0]
+        name = row[1]
+        first_name, last_name = name.split()
+        dob = row[2]
+        output_dob = datetime.datetime.strptime(dob, "%Y-%m-%d").strftime("%m/%d/%Y")
+        ssn = row[3]
+        output_ssn = "***-**-" + ssn.split('-')[2]
+        state = row[4]
+        state_abbrev = us_state_abbrev[state]
+        print(emp_id,name,first_name,last_name,dob,output_dob,ssn,output_ssn,state,state_abbrev)
